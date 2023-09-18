3ddcfe65072f4adb65a6e328861092a46532cabc
Павел Филиппов <pgphil@yandex.ru>
2023-09-18 23:42:10 +0300

    correct allowed_hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,14 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-# SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-
-# DEBUG = True
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ' ').split(',')
-
-# ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
